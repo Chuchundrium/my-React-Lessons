d75a544349244e7666971c06839dc1d49bcedc7d
Olga <Chuchundrium@users.noreply.github.com>
2020-03-01 02:12:09 +0200

    lesson49

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = (props) => {
   return (
@@ -19,12 +20,13 @@ const App = (props) => {
       <div className='app-wrapper-content'>
         <Route
           path='/profile'
-          render={() => <Profile/>} 
-        />
+          render={() => <Profile />} />
         <Route
           path='/dialogs'
-          render={() => <DialogsContainer/>}
-        />
+          render={() => <DialogsContainer />} />
+        <Route
+          path='/users'
+          render={() => <UsersContainer />} />
         <Route
           path='/news'
           render={() => <News />} />
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from "redux";
 import profileReducer from "./profile-reducer";
 import messagesReducer from "./messages-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import usersReducer from "./users-reducer";
 
 let allReducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
-    sidebar: sidebarReducer
+    sidebar: sidebarReducer,
+    usersPage: usersReducer
 });
 
 let store = createStore(allReducers);
