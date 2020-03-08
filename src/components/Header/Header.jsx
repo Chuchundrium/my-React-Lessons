d73c84431d31295c57e9c74d165ec77afb2d2bc8
Olga <Chuchundrium@users.noreply.github.com>
2020-03-08 20:06:23 +0200

    lesson61-cookie-authMe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-    return <header className={s.header}>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZyZ3midqAHWJTRUzs0OJseZGag35vedI3iqBccaTxBEkKi7II' />
-      </header>
+const Header = (props) => {
+  return <header className={s.header}>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZyZ3midqAHWJTRUzs0OJseZGag35vedI3iqBccaTxBEkKi7II' />
+    <div className={s.loginBlock}>
+      {
+        props.isAuth ? props.login
+          : <NavLink to={'/login'}>Login</NavLink>
+      }
+    </div>
+  </header>
 }
 
 export default Header;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import messagesReducer from "./messages-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import authReduser from "./auth-reducer";
 
 let allReducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReduser
 });
 
 let store = createStore(allReducers);
